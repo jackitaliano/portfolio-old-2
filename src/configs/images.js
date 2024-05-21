f960c12682a4e88f58c29e136c703b2a7dde57a7
jackitaliano <italiano.16@osu.edu>
2024-05-21 15:07:31 -0500

    change assset host to just github instead of imgur
    - can load images from raw github, so don't have to host on imgur

--- src/configs/images.js
+++ src/configs/images.js
@@ -1,86 +1,86 @@
 export const IMAGES = {
 	headshot: {
-		url: "https://i.imgur.com/OIdbLjE.png",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/headshot.jpeg",
 		loading: "static/images/headshot-min.jpeg",
 		alt: "headshot of Jack"
 	},
 	nebula: {
-		url: "https://i.imgur.com/NEst8fU.jpg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/nebula.jpeg",
 		loading: "static/images/nebula-min.jpeg",
 		alt: "nebula photo from James Webb"
 	},
 	fountainGroup: { 
-		url: "https://i.imgur.com/Q5Yh74S.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/fountainGroup.jpeg",
 		loading: "static/images/fountainGroup-min.jpeg",
 		alt: "NASA Interns by the Fountain"
 	},
 	sfSign: { 
-		url: "https://i.imgur.com/3e9Canx.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/sfSign.jpeg",
 		loading: "static/images/sfSign-min.jpeg",
 		alt: "Jack by the State Farm sign"
 	},
 	sfGroup: { 
-		url: "https://i.imgur.com/nnnOVkg.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/sfGroup.jpeg",
 		loading: "static/images/sfGroup-min.jpeg",
 		alt: "Jack by with State Farm Interns"
 	},
 	thinkAndDrinkDemo: { 
-		url: "https://i.imgur.com/AbGLX0j.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/thinkAndDrinkDemo.jpeg",
 		loading: "static/images/thinkAndDrinkDemo-min.jpeg",
 		alt: "'Think and Drink' demo at History Museum"
 	},
 	hike: { 
-		url: "https://i.imgur.com/2gDM3oJ.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/hike.jpeg",
 		loading: "static/images/hike-min.jpeg",
 		alt: "Hike with NASA Interns"
 	},
 	astronaut: { 
-		url: "https://i.imgur.com/FMXIJx5.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/astronaut.jpeg",
 		loading: "static/images/astronaut-min.jpeg",
 		alt: "Jack with an astronaut"
 	},
 	engineering: { 
-		url: "https://i.imgur.com/AkSlAcp.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/engineering.jpeg",
 		loading: "static/images/engineering-min.jpeg",
 		alt: "Jack with engineering group"
 	},
 	glennFront: {
-		url: "https://i.imgur.com/KibR3oW.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/glennFront.jpeg",
 		loading: "static/images/glennFront-min.jpeg",
 		alt: "Front of NASA Glenn"
 	},
 	hack: { 
-		url: "https://i.imgur.com/A5VHDSY.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/hack.jpeg",
 		loading: "static/images/hack-min.jpeg",
 		alt: "Hack OH/IO Jack's Freshman year"
 	},
 	makeCar: { 
-		url: "https://i.imgur.com/iHT9plW.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/makeCar.jpeg",
 		loading: "static/images/makeCar-min.jpeg",
 		alt: "Jack's Make OH/IO Project car"
 	},
 	hikingJaclyn: { 
-		url: "https://i.imgur.com/BIXEPzB.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/hikingJaclyn.jpeg",
 		loading: "static/images/hikingJaclyn-min.jpeg",
 		alt: "Jack hiking with Jaclyn"
 	},
 	skiSibs: { 
-		url: "https://i.imgur.com/TylUVTL.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/skiSibs.jpeg",
 		loading: "static/images/skiSibs-min.jpeg",
 		alt: "Jack skiing with siblings"
 	},
 	snowboardMe: { 
-		url: "https://i.imgur.com/jGUtMnc.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/snowboardMe.jpeg",
 		loading: "static/images/snowboardMe-min.jpeg",
 		alt: "Jack snowboarding"
 	},
 	pilam: { 
-		url: "https://i.imgur.com/7I8GhJ0.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/pilam.jpeg",
 		loading: "static/images/pilam-min.jpeg",
 		alt: "Pilam House"
 	},
 	buckeyes: { 
-		url: "https://i.imgur.com/4mZEBLK.jpeg",
+		url: "https://raw.githubusercontent.com/jackitaliano/portfolio/main/assets/images/buckeyes.jpeg",
 		loading: "static/images/buckeyes-min.jpeg",
 		alt: "Buckeyes Football Game"
 	},
